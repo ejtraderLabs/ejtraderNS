70010758317d8657b4565af9d1d25d474714c662
Emerson Pedroso <emerson@bitcoinnano.org>
2023-05-05 18:36:16 -0300

    fix topic

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
-        "Topic :: Office/Business :: Financial :: News",
         "Topic :: Office/Business :: Financial :: Investment",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries"
